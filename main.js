a05b43c1cfa8731690a2ca748c370a07a21ad68d
Chris Allick <chrisallick@gmail.com>
2015-01-26 17:19:03 -0500

    added as rollover

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 $(document).ready(function() {
     var products = JSON.parse($("head #products").text());
 
-    $(".brick").each(function(index,value){
-        if( products[index].title && products[index].price_category.range_min ) {
+    $(".brick").each(function(index,value) {
+        if( products[index] && products[index].title && products[index].price_category.range_min ) {
             var div = document.createElement("div");
 
-            console.log( $(this) );
-            
-            div.class = "brick-overlay";
             $(div).css({
-                background: "rgba(255,255,255,.8)",
+                background: "rgba(0,0,0,1)",
                 position: "absolute",
-                top: 0,
+                bottom: 0,
                 left: 0,
                 width: $(this).width(),
                 height: 60,
-                color: "#000000",
-                "z-index": 10000
-            }).html("<p>"+products[index].title+"</p><p>$"+products[index].price_category.range_min+"</p>");
+                color: "#FFFFFF",
+                "z-index": 10000,
+                display: "none"
+            }).addClass("brick-overlay").html("<p>"+products[index].title+"</p><p>$"+products[index].price_category.range_min+"</p>");
             $("p", div).css({
                 margin: 0
             });
             
             $(this).append(div);
         }
+
+        $(this).mouseover(function(){
+            $(".brick-overlay", this).show();
+        }).mouseout(function(){
+            $(".brick-overlay", this).hide();
+        });
     });
 });
